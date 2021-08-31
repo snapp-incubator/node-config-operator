2becc033c32b0569281542c4ec4170aedfe116ab
Mohammad Yosefpor <myusefpur@gmail.com>
2021-09-01 00:13:35 +0430

    initial commit

---
+++ api/v1alpha1/nodeconfig_types.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NodeConfigSpec defines the desired state of NodeConfig
+type NodeConfigSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of NodeConfig. Edit nodeconfig_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// NodeConfigStatus defines the observed state of NodeConfig
+type NodeConfigStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// NodeConfig is the Schema for the nodeconfigs API
+type NodeConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NodeConfigSpec   `json:"spec,omitempty"`
+	Status NodeConfigStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// NodeConfigList contains a list of NodeConfig
+type NodeConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NodeConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NodeConfig{}, &NodeConfigList{})
+}
